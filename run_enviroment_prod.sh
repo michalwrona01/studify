ebe447e0c8b3f6f815616dde777d79dfe3042acf
Michał Wrona <wronamichal01@gmail.com>
2025-09-18 23:41:03 +0200

    Update

--- run_enviroment_prod.sh
+++ run_enviroment_prod.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Starting the enviroment..."
-VAULT="vault/docker-compose.yaml"
-BACKEND="backend/docker-compose_prod.yaml"
-BOT="bot/docker-compose_prod.yaml"
+VAULT="vault/docker-compose-prod.yaml"
+BACKEND="backend/docker-compose-prod.yaml"
+BOT="bot/docker-compose-prod.yaml"
 
 
 docker-compose -f $VAULT -f $BACKEND -f $BOT up
