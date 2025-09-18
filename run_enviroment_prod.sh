66a1282efe11e0b4eb74e4e0e73191c188cf4ff5
Michał Wrona <wronamichal01@gmail.com>
2025-09-19 00:27:23 +0200

    Update run env

--- run_enviroment_prod.sh
+++ run_enviroment_prod.sh
@@ -5,5 +5,8 @@ VAULT="vault/docker-compose-prod.yaml"
 BACKEND="backend/docker-compose-prod.yaml"
 BOT="bot/docker-compose-prod.yaml"
 
+echo $VAULT
+echo $BACKEND
+echo $BOT
 
 docker compose -f $VAULT -f $BACKEND -f $BOT up
